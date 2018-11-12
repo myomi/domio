f2e7f609b45190a1f55287555397f5379e96e0ef
myomi <myomit@gmail.com>
2018-11-12 19:09:06 +0900

    prevent img tag's drop

--- sample/drag-drop.ts
+++ sample/drag-drop.ts
@@ -1,37 +1,55 @@
-import Domio from '../src/Domio';
-import './drag-drop.scss';
+import Domio from "../src/Domio";
+import "./drag-drop.scss";
 
-const dropArea = document.querySelector('#dropArea') as HTMLInputElement;
+const dropArea = document.querySelector("#dropArea") as HTMLInputElement;
 
-dropArea.addEventListener('dragenter', (event) => {
+dropArea.addEventListener(
+  "dragenter",
+  event => {
     event.stopPropagation();
     event.preventDefault();
-    dropArea.classList.add('hover');
-}, false);
+    dropArea.classList.add("hover");
+  },
+  false
+);
 
-dropArea.addEventListener('dragover', (event) => {
+dropArea.addEventListener(
+  "dragover",
+  event => {
     event.stopPropagation();
     event.preventDefault();
-}, false);
+  },
+  false
+);
 
-dropArea.addEventListener('dragleave', (event) => {
+dropArea.addEventListener(
+  "dragleave",
+  event => {
     event.stopPropagation();
     event.preventDefault();
+  },
+  false
+);
 
-}, false);
-
-dropArea.addEventListener('drop', async (event) => {
+dropArea.addEventListener(
+  "drop",
+  async event => {
     event.stopPropagation();
     event.preventDefault();
-    dropArea.classList.remove('hover');
+    dropArea.classList.remove("hover");
 
     const files = event.dataTransfer.files;
+    if (files.length === 0) {
+      return;
+    }
     const file = files[0];
     const dFile = await Domio.fromFile(file);
     const resized = await Domio.resize(dFile, 100);
-    const container = document.querySelector('#fileInformation');
+    const container = document.querySelector("#fileInformation");
     container.textContent = JSON.stringify(resized);
-    const img = document.createElement('img');
+    const img = document.createElement("img");
     img.src = resized.dataURL.src;
     dropArea.appendChild(img);
-}, false);
+  },
+  false
+);
